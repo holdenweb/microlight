fcedbc6abbe5fbbffd97be9e27f09d60a04fddb6
Steve Holden <steve@holdenweb.com>
2016-08-14 11:04:54 +0100

    Attempt to provide minimum reproducible case. REPL is still available after import, but locks up after some time.

---
+++ test1.py
@@ -0,0 +1,14 @@
+import micropython
+from machine import Timer
+micropython.alloc_emergency_exception_buf(100)
+
+index = 0
+
+def cb(t):
+    global index
+    index += 1
+    return
+
+tim4 = Timer(1, mode=Timer.PERIODIC, width=16)
+chan = tim4.channel(Timer.A, freq=10)
+chan.irq(trigger=Timer.TIMEOUT, handler=cb)
